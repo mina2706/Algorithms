0b3511bdd60e13bde4b8739e1f5ead11994fbdea
HW2025 <maachaa@etu.univ-grenoble-alpes.fr>
2025-12-10 20:33:50 +0000

    Ajout de la fonction traceTriangleEquilat pour dessiner un triangle équilatéral

--- TDs/TD11/utils.py
+++ TDs/TD11/utils.py
@@ -3,3 +3,8 @@ def traceCarre2 (lg , t):
     for _ in range(4):
         t.forward(lg)
         t.left(90)
+
+def traceTriangleEquilat (lg , t):
+    for _ in range(3):
+        t.forward(lg)
+        t.left(120)
