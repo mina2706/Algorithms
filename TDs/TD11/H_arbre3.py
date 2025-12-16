09db5db7971d34d39ce28be940851e1b8a53d157
Maacha A <maachaa@etu.univ-grenoble-alpes.fr>
2025-12-16 20:52:03 +0100

    Tracé HArbre - V3 : trace le H de base au fur et à mesure de la recursion

--- TDs/TD11/H_arbre3.py
+++ TDs/TD11/H_arbre3.py
@@ -1,31 +1,23 @@
 '''
     Version 2 de l'action recursive hArbre en se servant de la symétrie
+    Il trace le H principal au fur et à mesure de la recursion
 '''
 import turtle
-from interface_machine_trace.machine_trace import creer_fenetre_algo
 import time
+from interface_machine_trace.machine_trace import creer_fenetre_algo
 
-def hArbre2(t, n, lg):
-    for _ in range(2):  # répéter pour les deux moitiés la droite puis la gauche
-        t.forward(lg/2)
-        t.left(90)
-        t.forward(lg/2)
-        t.backward(lg)
-        t.forward(lg/2)
-        t.left(90)
-        t.forward(lg/2)
-
-    if n>1 :
+def hArbre3(t, n, lg):
+    if n>=1 :
         for _ in range(2):  # répéter pour les deux moitiés la droite puis la gauche
             t.forward(lg/2)
             t.left(90)
             t.forward(lg/2)
             t.right(90)
-            hArbre2(t, n-1, lg/2)
+            hArbre3(t, n-1, lg/2)
             t.left(90)
             t.backward(lg)
             t.right(90)
-            hArbre2(t, n-1, lg/2)
+            hArbre3(t, n-1, lg/2)
             t.left(90)
             t.forward(lg/2)
             t.left(90)
@@ -33,6 +25,6 @@ def hArbre2(t, n, lg):
 
 if __name__ == "__main__":
     debut = time.time()
-    creer_fenetre_algo(hArbre2, params={'n': '','lg':''})
+    creer_fenetre_algo(hArbre3, params={'n': '','lg':''})
     fin = time.time()
     print(f"Temps d'exécution : {fin - debut} secondes")
